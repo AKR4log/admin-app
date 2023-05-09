f913df64d7a13a116824d5a213ca5ec355482ee8
Grigoriev Aleksander <akr4log@gmail.com>
2023-05-09 20:25:47 +0300

    FIX

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-"use client";
-
-import { useTypedSelector } from "@/lib/hooks/useTypedSelector";
 import LoginPage from "@/pages/login/page";
-import { Main } from "next/document";
 
 export default async function Home() {
-  const auth = useTypedSelector((state) => state.auth.auth);
-  return <main>{auth.length !== 0 ? <Main /> : <LoginPage />}</main>;
+  return <main><LoginPage /></main>;
 }
